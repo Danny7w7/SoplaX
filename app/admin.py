8e18062a0fcb6aae6f797c67385d24ba66db836a
Danny Zabaleta <99416451+Danny7w7@users.noreply.github.com>
2024-06-14 21:58:43 -0500

    Otra vez mi web :D

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Videos, Usuario
 
 # Register your models here.
+admin.site.register(Videos)
+admin.site.register(Usuario)
--- app/forms.py
+++
@@ -1,7 +0,0 @@
-from .models import Client
-from django import forms
-
-class ClientForm(forms.ModelForm):
-    class Meta:
-        model = Client
-        fields = '__all__'
